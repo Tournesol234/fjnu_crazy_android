b778b6f5351c38909fd8e22197726fdd89d39a44
2199671574@qq.com <2199671574@qq.com>
2024-10-31 11:34:38 +0800

    homework02

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,77 +1,65 @@
 package com.example.myfirstapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import com.example.myfirstapplication.databinding.ActivityMainBinding;
+public class MainActivity extends Activity {
 
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private List<Map<String, Object>> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // 假设这是你的布局文件，其中包含了ListView控件
+        setContentView(R.layout.activity_main);
 
-        setSupportActionBar(binding.toolbar);
+        listView = findViewById(R.id.listView);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // 创建数据源
+        String[] animals = {"Lion", "Tiger", "Monkey", "Dog", "Cat"};
+        int[] images = {R.drawable.lion, R.drawable.tiger, R.drawable.monkey, R.drawable.dog, R.drawable.cat};
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // 将数据和图片放入Map集合中
+        dataList = new ArrayList<>();
+        for (int i = 0; i < animals.length; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", animals[i]);
+            map.put("image", images[i]);
+            dataList.add(map);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        // 创建SimpleAdapter适配器
+        adapter = new SimpleAdapter(this, dataList,
+                R.layout.simpleadapter, // 每个列表项的布局文件
+                new String[]{"name", "image"}, // 数据源的键名
+                new int[]{R.id.nameTextView, R.id.imageView}); // 对应的视图ID
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        // 设置适配器到ListView
+        listView.setAdapter(adapter);
+
+        // 为ListView设置点击事件监听器
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 获取被点击的项的数据
+                Map<String, Object> itemData = dataList.get(position);
+
+                // 显示一个Toast消息框，包含选中的动物名称
+                Toast.makeText(MainActivity.this, "You selected: " + itemData.get("name"), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
